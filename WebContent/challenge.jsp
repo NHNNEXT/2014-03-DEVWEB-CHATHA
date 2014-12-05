5603d4a0598d963c71ab38a73f50051ea44c6f58
unitimes <unitimes@naver.com>
2014-12-05 16:38:47 +0900

    resolved #18

---
+++ WebContent/challenge.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Challenge</title>
+</head>
+<body>
+<form action="/sendChallenge" method="post">
+<input type="text" name="pid" id="pid">
+<input type="submit" value="도전">
+</form>
+</body>
+</html>
---
+++ src/realrank/support/MessageMod.java
@@ -0,0 +1,51 @@
+package realrank.support;
+
+public class MessageMod {
+	private String from;
+	private String to;
+	private String subject;
+	private String contents;
+	
+	public MessageMod() {};
+	
+	public MessageMod(String from, String to, String subject, String contents) {
+		super();
+		this.from = from;
+		this.to = to;
+		this.subject = subject;
+		this.contents = contents;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	
+}
