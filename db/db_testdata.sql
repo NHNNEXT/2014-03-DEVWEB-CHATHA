0a7308bd4bd8b1830cd23dd03acb65c042846e4c
Lee JinWoo <jinus.kr@gmail.com>
2014-12-11 22:24:32 +0900

    Battle test data

--- db/db_testdata.sql
+++ db/db_testdata.sql
@@ -13,16 +13,18 @@ INSERT INTO `score` VALUES
 	('champ', 0),
 	('chal', 0);
 
--- state field: 0(acceptable and not validated) 1(canceled) 2(timeout) 3(canceled) 4(denied)
+-- state field: 0(acceptable and not validated) 1(accepted) 2(outdated) 3(canceled) 4(denied)
 DELETE FROM `battle`;
 INSERT INTO `battle` VALUES
 	(NULL, 'chal', 'champ', '2014-12-09 09:00:00', NULL, 1, NULL),
 	(NULL, 'champ', 'chal', '2014-12-09 09:00:00', NULL, 1, NULL),
-	(NULL, 'chal', 'champ', '2014-12-10 10:00:00', NULL, 2, NULL),
-	(NULL, 'champ', 'chal', '2014-12-10 10:00:00', NULL, 2, NULL),
-	(NULL, 'chal', 'champ', '2014-12-11 11:00:00', NULL, 3, NULL),
-	(NULL, 'champ', 'chal', '2014-12-11 11:00:00', NULL, 3, NULL),
-	(NULL, 'chal', 'champ', '2014-12-12 12:00:00', NULL, 4, NULL),
-	(NULL, 'champ', 'chal', '2014-12-12 12:00:00', NULL, 4, NULL),
-	(NULL, 'chal', 'champ', CURDATE(), NULL, 0, NULL),
-	(NULL, 'champ', 'chal', CURDATE(), NULL, 0, NULL);
+	(NULL, 'chal', 'champ', '2014-12-09 10:00:00', NULL, 2, NULL),
+	(NULL, 'champ', 'chal', '2014-12-09 10:00:00', NULL, 2, NULL),
+	(NULL, 'chal', 'champ', '2014-12-09 11:00:00', NULL, 3, NULL),
+	(NULL, 'champ', 'chal', '2014-12-09 11:00:00', NULL, 3, NULL),
+	(NULL, 'chal', 'champ', '2014-12-09 12:00:00', NULL, 4, NULL),
+	(NULL, 'champ', 'chal', '2014-12-09 12:00:00', NULL, 4, NULL),
+	(NULL, 'chal', 'champ', '2015-12-09 09:00:00', NULL, 0, NULL),
+	(NULL, 'chal', 'champ', '2015-12-10 09:00:00', NULL, 0, NULL),
+	(NULL, 'champ', 'chal', '2015-12-09 09:00:00', NULL, 0, NULL),
+	(NULL, 'champ', 'chal', '2015-12-10 09:00:00', NULL, 0, NULL);
