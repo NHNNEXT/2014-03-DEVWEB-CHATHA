23dd3867ae3c3eedf47844a8c9b8a05b0a2b647b
Laon618 <hotkids@hotmail.co.kr>
2015-01-03 13:14:50 +0900

    UserManager debug. use QueryExecuter instead of DBMethods.

--- src/realrank/user/UserManager.java
+++ src/realrank/user/UserManager.java
@@ -2,11 +2,15 @@ package realrank.user;
 
 import java.util.List;
 
+import easyjdbc.query.QueryExecuter;
 import realrank.objects.User;
-import easyjdbc.dao.DBMethods;
+
 
 public class UserManager {
 	public static List<User> getUserByIdOrEmail(String query) {
-		return DBMethods.getList(User.class, "id like ? or email like ?", "%"+query+"%", "%"+query+"%");
+		QueryExecuter qe = new QueryExecuter();
+		List<User> userList = qe.getList(User.class, "id like ? or email like ?", "%"+query+"%", "%"+query+"%");
+		qe.close();
+		return userList;
 	}
 }
