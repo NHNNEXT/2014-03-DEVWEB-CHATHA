489797651b825284e67d0b3b0564e5214183bfda
Laon618 <hotkids@hotmail.co.kr>
2015-01-04 15:21:49 +0900

    battleAccpetTest.

---
+++ test/realrank/battle/battleAcceptTest.java
@@ -0,0 +1,53 @@
+package realrank.battle;
+
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import easyjdbc.query.GetRecordQuery;
+import easyjdbc.query.QueryExecuter;
+import easymapping.mapping.Http;
+import realrank.controller.BattleController;
+import realrank.objects.Battle;
+import realrank.objects.User;
+
+public class battleAcceptTest {
+
+	BattleController con = new BattleController();
+	Http http = Mockito.mock(Http.class);
+
+	
+	@Test
+	public void acceptTest(){
+		Battle battle = new Battle();
+		battle.setId(10);
+		battle.setChallenger("chal");
+		User user = new User();
+		user.setId("champ");
+		Mockito.when(http.getJsonObject(Battle.class, "battle")).thenReturn(battle);
+		Mockito.when(http.getSessionAttribute(User.class,  "user")).thenReturn(user);
+		
+		con.acceptChallenge(http);
+	}
+	
+	@Test
+	public void getTime(){
+		GetRecordQuery que = new GetRecordQuery(1, "select now()");
+		QueryExecuter qe = new QueryExecuter();
+		System.out.println(qe.execute(que));
+	//	con.acceptChallenge(http);
+	}
+	
+	public void denyTest(){
+		Battle battle = new Battle();
+		battle.setId(10);
+		battle.setChallenger("chal");
+		User user = new User();
+		user.setId("champ");
+		Mockito.when(http.getJsonObject(Battle.class, "battle")).thenReturn(battle);
+		Mockito.when(http.getSessionAttribute(User.class,  "user")).thenReturn(user);
+		
+		con.denyChallenge(http);
+	}
+	
+}
